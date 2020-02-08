bd5fc375541d021f75295f7f75c1501d79909c99
ramp4 <rampatv13@gmail.com>
2020-02-08 19:05:42 +0300

    fix: price class selector

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-document.querySelectorAll("price").forEach(node => {
+document.querySelectorAll(".card-price").forEach(node => {
+  console.log(node);
   node.textContent = new Intl.NumberFormat("ru-RU", {
     currency: "rub",
     style: "currency"
