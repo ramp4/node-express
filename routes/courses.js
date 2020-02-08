c809a4a31eb108ec0dcc7dded00cfac17ebafd71
ramp4 <rampatv13@gmail.com>
2020-02-08 16:41:13 +0300

    implement start versions of routes, views, public, create course model, apply fs

---
+++ routes/courses.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const Courses = require("../models/course");
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const courses = await Courses.getAll();
+  res.render("courses", {
+    title: "Courses",
+    isCourses: true,
+    courses
+  });
+});
+
+module.exports = router;
